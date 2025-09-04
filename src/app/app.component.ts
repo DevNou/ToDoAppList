e568cb61e4dfebc504549c88c86cbdae69ab42ce
Arsenis Chatziantoniou <arsenisxatzi@hotmail.com>
2025-09-04 16:52:33 +0300

    End task some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodolistComponent } from './todolist/todolist.component';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TodolistComponent],
+  imports: [RouterOutlet,TodolistComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
